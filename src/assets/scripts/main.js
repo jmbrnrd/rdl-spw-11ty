4e8e63c5584c75817978514ffff1d2110b0852cf
James Barnard <jmbarnard@gmail.com>
2022-05-13 16:42:00 +0100

    Add js mods

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,7 +2,8 @@ import bookings from "./bookings";
 import map from "./map";
 import gallery from "./gallery";
 import formatting from "./formatting";
-
+import nav from './nav';
+nav();
 formatting();
 bookings();
 gallery();
