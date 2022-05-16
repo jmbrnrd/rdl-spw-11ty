d2024288048623399f74c94998d0f50d3790100e
James Barnard <jmbarnard@gmail.com>
2022-05-16 16:19:46 +0100

    env and switched data

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
 module.exports = function (config) {
   config.addWatchTarget("./src/assets/sass/");
   config.addShortcode("year", () => `${new Date().getFullYear()}`);
-  config.addWatchTarget('./src/assets/scripts/')
+  config.addWatchTarget('./src/assets/scripts/');
   return {
     //templateFormats: ['md', '11ty.js'],
     dir: {
       input: "src",
-      output: "public"
+      output: "public",
+      data: process.env.MY_ENVIRONMENT === "production" ? "_data-prod" : "_data-dev"
     },
   };
 };
