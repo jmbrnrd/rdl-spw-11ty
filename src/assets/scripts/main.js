bc534dea4ad20e66ee50473d989cc6c733216718
jmbrnrd <jmbarnard@gmail.com>
2022-05-18 08:33:40 +0100

    menus and lightbox wip

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,11 +1,10 @@
 import bookings from "./bookings";
 import map from "./map";
-import gallery from "./gallery";
 import formatting from "./formatting";
 import nav from './nav';
+
 nav();
 formatting();
 bookings();
-gallery();
 map();
 
