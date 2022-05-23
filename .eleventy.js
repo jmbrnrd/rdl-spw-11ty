4f2bb8e95a97e96d57dbb341108831127c3ab985
James Barnard <jmbarnard@gmail.com>
2022-05-23 17:33:27 +0100

    Add Glightbox to curated template

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (config) {
   config.addWatchTarget("./src/assets/sass/");
   config.addShortcode("year", () => `${new Date().getFullYear()}`);
   config.addWatchTarget('./src/assets/scripts/');
+  config.addPassthroughCopy("./src/assets/imgs/");
   return {
     //templateFormats: ['md', '11ty.js'],
     dir: {
