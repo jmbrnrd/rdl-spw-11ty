37cda5551ca0cc6460273033b95c25ee06a15fad
jmbrnrd <jmbarnard@gmail.com>
2022-09-13 09:10:15 +0100

    Offers

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,7 +3,7 @@ import map from "./map";
 import formatting from "./formatting";
 import nav from './nav';
 import gallery from './gallery';
-import offers from './offers'
+//import offers from './offers'
 
 
 nav();
