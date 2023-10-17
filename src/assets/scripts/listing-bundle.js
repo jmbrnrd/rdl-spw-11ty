d19b35cbe7356ee0639c00a1754cf38a766871f1
James Barnard <jmbarnard@gmail.com>
2023-10-17 14:41:07 +0100

    Add fr email booking request

--- src/assets/scripts/listing-bundle.js
+++ src/assets/scripts/listing-bundle.js
@@ -2,7 +2,7 @@ import map from "./map-lazy";
 import formatting from "./formatting";
 import claim from './claim';
 import gallery from './gallery';
-import bookingRequest from './booking-request';
+import bookingRequest from './booking-email';
 import about from "./about";
 
 formatting()
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,10 @@
-import bookings from "./bookings-mozrest";
+import bookings from "./booking-mozrest";
 import map from "./map-lazy";
 import formatting from "./formatting";
 import nav from './nav';
 import gallery from './gallery';
 import promotions from './promotions'
-import bookingRequest from './booking-request';
+import bookingRequest from './booking-email';
 import about from "./about";
 
 bookings();
