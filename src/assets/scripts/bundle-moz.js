d0d9f70d5be12b8a6475aa879dbada71639d3fb0
James Barnard <jmbarnard@gmail.com>
2023-12-08 19:27:32 +0000

    logoz

---
+++ src/assets/scripts/bundle-moz.js
@@ -0,0 +1,25 @@
+if (process.env.NODE_ENV === 'production') {
+    console.log = () => {};
+}
+
+import bookings from "./booking-mozrest";
+import map from "./map-lazy";
+import formatting from "./formatting";
+import nav from './nav';
+import gallery from './gallery';
+import promotions from './promotions'
+import bookingRequest from './booking-email';
+import about from "./about";
+// import events from './events';
+
+
+formatting();
+map();
+gallery();
+bookingRequest();
+about();
+
+bookings();
+// events();
+promotions();
+nav();
